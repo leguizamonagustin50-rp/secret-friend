a54e8f1e9778cd5ee9b7af862399c1044fb10676
leguizamonagustin50-rp <leguizamonagustin50@gmail.com>
2025-08-26 13:35:48 -0300

    agregar amigo con id

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let agregarAmigo = [];
+let sumarAmigo = [];
 
 function agregarAmigo() {
-  let nombre = prompt("Ingrese el nombre del amigo:");
-  let edad = parseInt(prompt("Ingrese la edad del amigo:"));
-
-  if (edad < 18) {
-    alert("No se pueden agregar amigos menores de edad.");
-  } else {
-    agregarAmigo.push({ nombre: nombre, edad: edad });
-    alert("Amigo agregado exitosamente.");
-  }
+    let sumarAmigo = document.getElementById("amigo");
+  
 }
 
