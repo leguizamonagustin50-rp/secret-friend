8b9e9574ac1542621c3321c2ad06a33af625dc99
leguizamonagustin50-rp <leguizamonagustin50@gmail.com>
2025-08-26 22:22:14 -0300

    agragar amigo 1.0

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 let sumarAmigo = [];
 
 function agregarAmigo() {
-    let sumarAmigo = document.getElementById("amigo");
-  
+  let amigo = prompt("Ingrese el nombre de su amigo:");
+  if (amigo) {
+    sumarAmigo.push(amigo);
+    console.log(`Amigo ${amigo} agregado.`);
+  } else {
+    console.log("No se ingresó ningún nombre.");
+  }
 }
 
